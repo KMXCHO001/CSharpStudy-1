317908cfcebcae584d8e6e6de06cfd403f921eb9
KMXCHO001 <167495753+KMXCHO001@users.noreply.github.com>
2024-05-08 10:27:50 +0900

    edit: 반복문

---
+++ Assets/Scripts/IterationStudy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IterationStudy : MonoBehaviour
+{
+    //for
+    //foreach
+    string[] names = { "신태욱", "김형민", "김현수" };
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* //1. 초기화 2. 조건 3. 증감
+        for (int i = 0; i < 10; i++)
+        {
+            print(i);
+        }
+        // 0 ~ 배열의 길이까지 반복
+        for (int i = 0; i < names.Length; i++)
+        {
+            print(names[i]);
+
+        }
+
+        for (int i = names.Length - 1; i >= 0; i--)
+        {
+            print(names[i]);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+                print(i);
+        }
+        // 조건을 입력할 필요가 없음
+        foreach (string name in names)
+            print(name);
+
+        //while
+        int number = 0;
+        while (number < 10)
+        {
+            number++;
+        }
+
+        while (true)
+        {
+            if (number == 10)
+            {
+                break; //반복문 종료
+            }
+        }
+
+        //do while
+        int num = 0;
+        do
+        {
+            print(num);
+            num++;
+
+        }
+        while (num < 10);*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
