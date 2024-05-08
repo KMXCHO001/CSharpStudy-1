c85168ed4e16b543b1111c088304e1076de5204d
KMXCHO001 <167495753+KMXCHO001@users.noreply.github.com>
2024-05-08 17:39:04 +0900

    edit: class

--- Assets/Scripts/ClassStudy.cs
+++ Assets/Scripts/ClassStudy.cs
@@ -1,87 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Person
-{
-    public string name;
-    public int age;
-
-    public Person(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
-    }
-
-    // method
-    //접근지정자, 리턴형(void), 클래스 이름(speak)
-    public void Speak()
-    {
-
-    }
-
-    public string Speak(string message) //메서드 오버로드 - 다형성 예
-    {
-        return message;
-    }
-}
-
-public class Vehicle
-{
-    public void Honk()
-    {
-
-    }
-}
-
-public class Car : Vehicle
-{   
-    public Car()
-    {
-
-    }
-    public void Accelerate()
-    {
-        this.Honk();
-    }
-}
-
-public class Truck : Vehicle
-{
-    public void Accelerate()
-    {
-        this.Honk();
-    }
-}
-
-public class Bus : Vehicle
-{
-    public void Accelerate()
-    {
-        this.Honk();
-    }
-}
 
 public class ClassStudy : MonoBehaviour
-{   
-    
-    void Start()
+{
+    [SerializeField] TMP_InputField vehicleNumberInput;
+    [SerializeField] Button registerBtn;
+
+    public class Car
     {
-        Person p1 = new Person("김철희", 29);
-        //p1.name = "김철희"; //필드 값을 대입
-        //p1.age = 29;
+        public string carnumber;
+        public DateTime date;
+        public Car(int carNumber, DateTime date)
+        {
+            this.carNumber = carNumber;
+            this.date = date;
+        }
 
-        print(p1.Speak("Hello"));
-
-        //주차장 관리시스템
-        //1. Car 클래스 -> 차량번호, 입차시간을 필드로 가지는 클래스
-        //2. ParkingLot 클래스 -> Car클래스를 넣을 수 있는 컬렉션으로 자동차를 관리
-        //2-1. 차량 입차, 출차 기능(메서드), 현재 주차된 목록 출력 기능
     }
 
-    Car car = new Car();
-    
 
+    public class ParkingLot
+    {
+        List<Car> cars = new List<Car>();
+
+        public void Incar(string carNumber)
+        {
+            Car car = new Car(carNumber,DateTime.Now);
+            cars.Add(car);
+        }
+
+        public void Outcar()
+        {
+            Car carFound = cars.Find(x => x.carnumber == carNumber);
+            cars.Remove(carFound);
+        }
+
+        public void ShowCarList()
+        {
+
+        }
+
+        ParkingLot parkingLot;
+
+        void Start()
+        {
+            parkingLot car = new ParkingLot();
+            parkingLot.Incar("5555");
+            parkingLot.ShowCarList();
+
+            //주차장 관리시스템
+            //1. Car 클래스 -> 차량번호, 입차시간을 필드로 가지는 클래스
+            //2. ParkingLot 클래스 -> Car클래스를 넣을 수 있는 컬렉션으로 자동차를 관리
+            //2-1. 차량 입차, 출차 기능(메서드), 현재 주차된 목록 출력 기능
+        }
+
+        public void OnRegisterBtnClkEvent()
+        {
+            print("parkingLot을 만들어 주세요.");
+            return;
+        }
+
+        parkingLot.Incar(vehicleNumberInput.text);
+        parkingLot.ShowCarList();
+
+       
+
+
+    }
 }
 
